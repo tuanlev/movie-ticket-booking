7fa60824152e807ff67ddf7da3a21613e6e30986
tuanle13-git <tuanlv1303@gmail.com>
2025-11-21 23:22:33 +0700

    feat(auth-service): add repository(AccountRepository)

--- app/auth-service/src/main/java/com/tuan/authservice/config/AppConfig.java
+++ app/auth-service/src/main/java/com/tuan/authservice/config/AppConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class AppConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
---
+++ app/auth-service/src/main/java/com/tuan/authservice/repository/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.tuan.authservice.repository;
+
+import com.tuan.authservice.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account,String> {
+    Optional<Account> findDistinctByGmail(String s);
+    Optional<Account> check(String s);
+}
